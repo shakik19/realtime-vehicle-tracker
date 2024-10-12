b6a7863580374217ad5b6c33aa01d26f8088f893
shakik19 <1ne9ine.o@gmail.com>
2024-10-12 06:22:32 +0600

    init

---
+++ src/main/java/bd/shakik/realtimebustracking/RealtimeBusTrackingApplication.java
@@ -0,0 +1,13 @@
+package bd.shakik.realtimebustracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeBusTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeBusTrackingApplication.class, args);
+	}
+
+}
