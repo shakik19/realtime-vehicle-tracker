88d599c7e5da6443a04aeda98c050f676b2f268f
shakik19 <1ne9ine.o@gmail.com>
2024-10-13 11:33:23 +0600

    init

--- src/main/java/bd/shakik/realtime/tracking/RealtimeBusTrackingApplication.java
+++ src/main/java/bd/shakik/realtime/tracking/RealtimeBusTrackingApplication.java
@@ -1,13 +1,11 @@
-package bd.shakik.realtimebustracking;
+package bd.shakik.realtime.tracking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RealtimeBusTrackingApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RealtimeBusTrackingApplication.class, args);
 	}
-
 }
--- src/test/java/bd/shakik/realtime/tracking/RealtimeBusTrackingApplicationTests.java
+++ src/test/java/bd/shakik/realtime/tracking/RealtimeBusTrackingApplicationTests.java
@@ -1,13 +1,13 @@
-package bd.shakik.realtimebustracking;
+package bd.shakik.realtime.tracking;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RealtimeBusTrackingApplicationTests {
-
+	
 	@Test
 	void contextLoads() {
 	}
-
+	
 }
