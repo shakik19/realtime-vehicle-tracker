9c579d08992c2fa2d9bbd3fd88e68768a7782b88
shakik19 <1ne9ine.o@gmail.com>
2024-10-20 15:33:45 +0600

    update: renamed

--- src/main/java/bd/shakik/realtime/tracking/App.java
+++ src/main/java/bd/shakik/realtime/tracking/App.java
@@ -2,12 +2,10 @@ package bd.shakik.realtime.tracking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
-@EnableKafka
-public class RealtimeVehicleTrackingApplication {
+public class App {
 	public static void main(String[] args) {
-		SpringApplication.run(RealtimeVehicleTrackingApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 }
